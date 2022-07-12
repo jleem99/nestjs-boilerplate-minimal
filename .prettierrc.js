5a5cb1372e77f3ebc692888f65517c53972369a1
jleem <jleem@gymblic.com>
2022-07-13 00:00:49 +0900

    chore: update prettier, eslint config

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	semi: false,
+	endOfLine: 'lf',
+	quoteProps: 'consistent',
+	arrowParens: 'always',
+	printWidth: 100,
+}
