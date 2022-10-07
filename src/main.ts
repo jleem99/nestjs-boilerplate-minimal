6d1fbb673f042654945ccc3c37d445f56bf25fbe
JongHan Leem <jleem42@gmail.com>
2022-10-08 01:04:57 +0900

    feat: respect PORT env variable

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ declare const module: any
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
 
+	await app.listen(process.env.PORT || 3000)
 	bootstrap_hmr(module, app)
 }
 
