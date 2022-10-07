dcea4c30f01195360a3973179ae21779cae5ea7e
JongHan Leem <jleem42@gmail.com>
2022-10-08 04:02:47 +0900

    feat: disable rate limiting

--- src/bootstrap-express.ts
+++ src/bootstrap-express.ts
@@ -1,5 +1,4 @@
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { rateLimit } from 'express-rate-limit'
 import helmet from 'helmet'
 import hpp from 'hpp'
 import { isProd } from './common/env-utils'
@@ -9,6 +8,7 @@ export function bootstrap_express(app: NestExpressApplication) {
 		// app.enable('trust proxy')
 		app.use(helmet({ contentSecurityPolicy: false }))
 		app.use(hpp())
-		app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
+		/** You can handle rate limit on reverse proxy */
+		// app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }))
 	}
 }
