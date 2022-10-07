4520216b2a9b6762ebe25214dd4814f7e8a2b7ac
JongHan Leem <jleem42@gmail.com>
2022-10-08 01:05:48 +0900

    feat: enable versioning

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { bootstrap_hmr } from './bootstrap-hmr'
@@ -6,6 +7,11 @@ declare const module: any
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	app.enableVersioning({
+		type: VersioningType.URI,
+		defaultVersion: '1',
+	})
 
 	await app.listen(process.env.PORT || 3000)
 	bootstrap_hmr(module, app)
